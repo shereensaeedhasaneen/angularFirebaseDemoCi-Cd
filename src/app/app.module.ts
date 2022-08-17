794fcb1e47acf6290fd77fc38804553b2d0d3e95
shereensaeedhasaneen <sheerysaeed123@gmail.com>
2022-08-17 17:19:23 +0200

    test rxjs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ParentComponent } from './components/parent/parent.component';
 import { ChildComponent } from './components/child/child.component';
 import { ConcatRxjsComponent } from './components/rxjs/concat-rxjs/concat-rxjs.component';
+import { ForkJoinRxjsComponent } from './components/rxjs/fork-join-rxjs/fork-join-rxjs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ParentComponent,
     ChildComponent,
-    ConcatRxjsComponent
+    ConcatRxjsComponent,
+    ForkJoinRxjsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/rxjs/fork-join-rxjs/fork-join-rxjs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fork-join-rxjs',
+  templateUrl: './fork-join-rxjs.component.html',
+  styleUrls: ['./fork-join-rxjs.component.scss']
+})
+export class ForkJoinRxjsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
